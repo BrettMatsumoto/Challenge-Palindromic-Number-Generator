8a37229ca43a6e4113449e37ab7eb25de4cdcc74
Brett <brett_matsumoto@yahoo.com>
2019-04-16 20:51:12 -1000

    completed challenge

--- palindromic-number-generator.js
+++ palindromic-number-generator.js
@@ -1,5 +1,27 @@
 function palindromicNumberGenerator(num) {
-  // do work here
-}
+  let reversed = Number(
+    num
+      .toString()
+      .split('')
+      .reverse()
+      .join(''),
+  );
+  let count = 0;
 
+  while (num !== reversed) {
+    num += reversed;
+    reversed = Number(
+      num
+        .toString()
+        .split('')
+        .reverse()
+        .join(''),
+    );
+    count++;
+  }
+  return {
+    value: num,
+    steps: count,
+  };
+}
 module.exports = palindromicNumberGenerator;
